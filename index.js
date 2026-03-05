1e24704f53996462fa96025f54338eb88211437e
daddyjudah <moderator647@gmail.com>
2026-03-05 18:08:17 +0000

    changed index.js

--- index.js
+++ index.js
@@ -12,24 +12,89 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-
+	const savedTasks = localStorage.getItem("tasks");
+	
+	if (savedTasks && JSON.parse(savedTasks).length > 0) {
+		return JSON.parse(savedTasks);
+	}
+	
+	return items;
 }
 
 function createItem(item) {
 	const template = document.getElementById("to-do__item-template");
 	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
+
   const textElement = clone.querySelector(".to-do__item-text");
   const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
   const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
+  textElement.textContent = item;
+	
+	deleteButton.addEventListener("click", () => {
+		clone.remove();
+		const updatedItems = getTasksFromDOM();
+		saveTasks(updatedItems);
+	});
+	
+	duplicateButton.addEventListener("click", () => {
+		const itemName = textElement.textContent;
+		const newItem = createItem(itemName);
+		listElement.prepend(newItem);
+		const updatedItems = getTasksFromDOM();
+		saveTasks(updatedItems);
+	});
+	
+	editButton.addEventListener("click", () => {
+		textElement.setAttribute("contenteditable", "true");
+		textElement.focus();
+	});
+	
+	textElement.addEventListener("blur", () => {
+		textElement.setAttribute("contenteditable", "false");
+		const updatedItems = getTasksFromDOM();
+		saveTasks(updatedItems);
+	});
+	
+	return clone;
+
 }
 
 function getTasksFromDOM() {
-
+	const itemsNamesElements = document.querySelectorAll(".to-do__item-text");
+	const tasks = [];
+	
+	itemsNamesElements.forEach((element) => {
+		tasks.push(element.textContent);
+	});
+	
+	return tasks;
 }
 
 function saveTasks(tasks) {
-
+	localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+formElement.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const taskText = inputElement.value;
+	
+	if (taskText.trim() === "") {
+		return;
+	}
+	
+	const newItem = createItem(taskText);
+	listElement.prepend(newItem);
+	items = getTasksFromDOM();
+	saveTasks(items);
+	inputElement.value = "";
+});
+
+
+items = loadTasks();
+
+items.forEach((item) => {
+	const newItem = createItem(item);
+	listElement.append(newItem);
+});
